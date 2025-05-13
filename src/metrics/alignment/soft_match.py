f821315e4f46ac1a05ee3fc20a29b341ddf9e1bf
ckapoor7 <chaitanya.7.kapoor@gmail.com>
2025-05-13 21:27:40 +0000

    add soft-matching distance

--- src/metrics/alignment/soft_match.py
+++ src/metrics/alignment/soft_match.py
@@ -1,27 +1,24 @@
 import ot
-import warnings
 import numpy as np
 from typing import Optional, List, Union
 
-from sklearn.model_selection import KFold
 from src.metrics.base import Metric
 
 import torch
 from torchmetrics.functional import pairwise_cosine_similarity
 
 
-class UnbalancedSoftMatch(Metric):
+class SoftMatch(Metric):
     """
-    compute a partial wasserstein transport plan to align a
-    distribution pair (eg: a pair of neural activations)
+    compute an optimal soft-permutation matrix to align a pair
+    of neural activations
     """
 
-    def __init__(self, mass_reg: Optional[float] = 1e-1, k_folds: Optional[int] = 5):
+    def __init__(self, k_folds: Optional[int] = 5):
         super().__init__(k_folds=k_folds)
-        self.mass_reg = mass_reg
 
     @staticmethod
-    def _clean_matrices(x: torch.Tensor) -> torch.Tensor:
+    def _clean_matrices(x: Union[torch.Tensor, np.ndarray]) -> torch.Tensor:
         """
         remove all 0-columns from x.
         """
@@ -37,7 +34,10 @@ class UnbalancedSoftMatch(Metric):
         x: Union[torch.Tensor, np.ndarray],
         y: Union[torch.Tensor, np.ndarray],
         **kwargs,
-    ) -> "UnbalancedSoftMatch":
+    ) -> "SoftMatch":
+        """
+        compute optimal soft-permutation matrix
+        """
         x = torch.tensor(x) if isinstance(x, np.ndarray) else x
         y = torch.tensor(y) if isinstance(y, np.ndarray) else y
 
@@ -57,9 +57,9 @@ class UnbalancedSoftMatch(Metric):
         a = torch.ones(x.shape[1], device=x.device) / x.shape[1]
         b = torch.ones(y.shape[1], device=x.device) / y.shape[1]
 
-        self.transform = ot.partial.partial_wasserstein(
-            a.cpu(), b.cpu(), self.cost.cpu(), m=float(self.mass_reg)
-        ).to(x_centered.device)
+        # compute optimal transport plan using earth movers distance
+        self.transform = ot.emd(a, b, self.cost).to(x.device)
+
         return self
 
     def score(
@@ -84,7 +84,6 @@ class UnbalancedSoftMatch(Metric):
             x = x.T - self.mx_
             y = y.T - self.my_
             similarity_score = 1 - torch.nansum(self.transform * self.cost).item()
-
         else:
             mx = torch.nanmean(x.T, dim=1)[:, None]
             my = torch.nanmean(y.T, dim=1)[:, None]
@@ -94,13 +93,23 @@ class UnbalancedSoftMatch(Metric):
             cost = 1 - pairwise_cosine_similarity(x_centered, y_centered)
             similarity_score = 1 - torch.nansum(self.transform * cost).item()
 
+        # assert that we have a birkhoff
+        norm_factor_row = 1 / self.transform.shape[0]
+        norm_factor_col = 1 / self.transform.shape[1]
+        row_sum, col_sum = torch.sum(self.transform, dim=1), torch.sum(
+            self.transform, dim=0
+        )
+        row_check = torch.all(torch.abs(row_sum - norm_factor_row) < 1e-3)
+        col_check = torch.all(torch.abs(col_sum - norm_factor_col) < 1e-3)
+        assert row_check.item()
+        assert col_check.item()
+
         return similarity_score
 
 
 if __name__ == "__main__":
-    x = torch.randn(100, 20).cuda()
-    y = torch.randn(100, 20).cuda()
+    x = torch.randn(1000, 128)
+    y = torch.randn(1000, 128)
 
-    metric = UnbalancedSoftMatch(mass_reg=0.5)
-
-    print(f"unbalanced metric: {metric.fit_kfold_score(x=x, y=y)}")
+    metric = SoftMatch()
+    print(f"kfold fit: {metric.fit_kfold_score(x, y)}")
