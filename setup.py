4703fd77c8413743ba2d9d304d793f67d31824c3
ckapoor7 <chaitanya.7.kapoor@gmail.com>
2026-01-29 01:30:48 +0000

    add README + conda environment file

--- setup.py
+++ setup.py
@@ -6,8 +6,6 @@ version = "0.1"
 setup(
     name=package,
     version=version,
-    description="unbalanced distance metric for neural representations",
+    description="partial distance metric for neural representations",
     packages=find_packages(),
-    #package_dir={'': 'src'},
-    #packages=find_packages(where="src"),
 )
